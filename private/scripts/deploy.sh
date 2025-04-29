6b859c187d168a4f8f0def4e9c9552ea17330afc
Chris Reynolds <chris@jazzsequence.com>
2025-04-29 11:48:28 -0600

    don't try to deploy live if test deploy failed

--- private/scripts/deploy.sh
+++ private/scripts/deploy.sh
@@ -3,4 +3,9 @@
 export TERMINUS_SITE=${TERMINUS_SITE:-"communitycodedev"}
 
 terminus env:deploy "$TERMINUS_SITE".test
+# if the last command ended in error, bail with a warning.
+if [ $? -ne 0 ]; then
+  echo "⚠️ Deploy to test failed. Skipping live deploy."
+  exit 1
+fi
 terminus env:deploy "$TERMINUS_SITE".live
