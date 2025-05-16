88d72bd346cef938ef70d3b1c11dd83ea5d923ad
Chris Reynolds <chris@jazzsequence.com>
2025-05-16 14:44:02 -0600

    deploy with commit messages

--- private/scripts/deploy.sh
+++ private/scripts/deploy.sh
@@ -2,9 +2,12 @@
 
 export TERMINUS_SITE=${TERMINUS_SITE:-"communitycodedev"}
 
+# Get the last commit message and store it in a variable
+LAST_COMMIT_MESSAGE=$(git log -1 --pretty=%B)
+
 # if the last command ended in error, bail with a warning.
-if ! terminus env:deploy "$TERMINUS_SITE".test; then
+if ! terminus env:deploy "$TERMINUS_SITE".test --note="Deploy to Test: ${LAST_COMMIT_MESSAGE}"; then
   echo "⚠️ Deploy to test failed. Skipping live deploy."
   exit 1
 fi
-terminus env:deploy "$TERMINUS_SITE".live
+terminus env:deploy "$TERMINUS_SITE".live --note="Deploy to Live: ${LAST_COMMIT_MESSAGE}"
