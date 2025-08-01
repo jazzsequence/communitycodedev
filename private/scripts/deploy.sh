d6f0809729b89d30b360fdb03f3a7b5acb8aae29
Chris Reynolds <chris@jazzsequence.com>
2025-08-01 12:22:22 -0600

    fix syntax errors

--- private/scripts/deploy.sh
+++ private/scripts/deploy.sh
@@ -20,8 +20,8 @@ done
 LAST_COMMIT_MESSAGE=$(git log -1 --pretty=%B)
 
 # if the last command ended in error, bail with a warning.
-if ! terminus env:deploy "$TERMINUS_SITE".test --note="Deploy to Test: ${LAST_COMMIT_MESSAGE}"; then
+if ! terminus env:deploy "$TERMINUS_SITE.test" --note="Deploy to Test: ${LAST_COMMIT_MESSAGE}"; then
   echo "⚠️ Deploy to test failed. Skipping live deploy."
   exit 1
 fi
-terminus env:deploy "$TERMINUS_SITE".live --note="Deploy to Live: ${LAST_COMMIT_MESSAGE}"
+terminus env:deploy "$TERMINUS_SITE.live" --note="Deploy to Live: ${LAST_COMMIT_MESSAGE}"
