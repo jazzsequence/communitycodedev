c6580dc51c4b90837f7a699691df20223cb02fb9
Chris Reynolds <chris@jazzsequence.com>
2025-09-09 09:11:11 -0600

    show prpl recommendations in menus

--- web/app/mu-plugins/cc-progress-planner.php
+++ web/app/mu-plugins/cc-progress-planner.php
@@ -14,6 +14,13 @@ namespace CommunityCode\ProgressPlanner;
 function init() {
     // Expose the Progress Planner CPTUI.
     add_filter( 'progress_planner_tasks_show_ui', '__return_true' );
+    add_filter( 'register_post_type_args', function( $args, $post_type ) {
+        if ( 'prpl_recommendations' === $post_type ) {
+            $args['show_ui']      = true;
+            $args['show_in_menu'] = true;
+        }
+        return $args;
+    }, 10, 2 );
 }
 
 add_action( 'init', __NAMESPACE__ . '\\init' );
