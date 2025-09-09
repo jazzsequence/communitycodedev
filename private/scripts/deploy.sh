378db698cfb13dbace8c5c2d57e10a19031b7806
Chris Reynolds <chris@jazzsequence.com>
2025-09-09 08:57:31 -0600

    move the for loop down to after we try to deploy test although this is really just for when we're deploying right after a push so maybe i need to just pull it out?

--- private/scripts/deploy.sh
+++ private/scripts/deploy.sh
@@ -7,6 +7,15 @@ STEPS_TO_WAIT=(
   "Build a slim image for test/live environment"
 )
 
+# Get the last commit message and store it in a variable
+LAST_COMMIT_MESSAGE=$(git log -1 --pretty=%B)
+
+# if the last command ended in error, bail with a warning.
+if ! terminus env:deploy "$TERMINUS_SITE.test" --note="Deploy to Test: ${LAST_COMMIT_MESSAGE}"; then
+  echo "⚠️ Deploy to test failed. Skipping live deploy."
+  exit 1
+fi
+
 # Wait for the specified workflows to complete
 for step in "${STEPS_TO_WAIT[@]}"; do
   echo "Waiting for $step to complete..."
@@ -16,12 +25,4 @@ for step in "${STEPS_TO_WAIT[@]}"; do
   fi
 done
 
-# Get the last commit message and store it in a variable
-LAST_COMMIT_MESSAGE=$(git log -1 --pretty=%B)
-
-# if the last command ended in error, bail with a warning.
-if ! terminus env:deploy "$TERMINUS_SITE.test" --note="Deploy to Test: ${LAST_COMMIT_MESSAGE}"; then
-  echo "⚠️ Deploy to test failed. Skipping live deploy."
-  exit 1
-fi
 terminus env:deploy "$TERMINUS_SITE.live" --note="Deploy to Live: ${LAST_COMMIT_MESSAGE}"
