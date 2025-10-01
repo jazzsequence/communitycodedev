958840ad77ff3ff998cd27694f4b59637f97fe01
Chris Reynolds <chris@jazzsequence.com>
2025-10-01 10:49:53 -0600

    spaces to tabs

--- web/app/mu-plugins/newrelic.php
+++ web/app/mu-plugins/newrelic.php
@@ -15,26 +15,26 @@ namespace CommunityCode\NewRelic;
  * Kick everything off.
  */
 function bootstrap() {
-    add_action( 'wp_head', __NAMESPACE__ . '\\add_newrelic_headers', 99 );
-    add_action( 'wp_footer', __NAMESPACE__ . '\\add_newrelic_footer', 99 );
+	add_action( 'wp_head', __NAMESPACE__ . '\\add_newrelic_headers', 99 );
+	add_action( 'wp_footer', __NAMESPACE__ . '\\add_newrelic_footer', 99 );
 }
 
 /**
  * Add New Relic headers to the page head.
  */
 function add_newrelic_headers() {
-    if ( function_exists( 'newrelic_get_browser_timing_header' ) ) {
-        echo newrelic_get_browser_timing_header();
-    }
+	if ( function_exists( 'newrelic_get_browser_timing_header' ) ) {
+		echo newrelic_get_browser_timing_header();
+	}
 }
 
 /**
  * Add New Relic footer to the page footer.
  */
 function add_newrelic_footer() {
-    if ( function_exists( 'newrelic_get_browser_timing_footer' ) ) {
-        echo newrelic_get_browser_timing_footer();
-    }
+	if ( function_exists( 'newrelic_get_browser_timing_footer' ) ) {
+		echo newrelic_get_browser_timing_footer();
+	}
 }
 
 // Bootstrap the plugin.
